0821f61e2ffbe243c3ccf354f14faf491b72dbf2
divyarajsinhsindhav-tss <divyarajsinh.sindhav@tssconsultancy.com>
2026-01-17 14:13:32 +0530

    Change in MatrixMultiplication

--- Basic/src/com/tss/test/MatrixOperation.java
+++ Basic/src/com/tss/test/MatrixOperation.java
@@ -3,82 +3,225 @@ package com.tss.test;
 import java.util.Scanner;
 
 public class MatrixOperation {
-    public static Scanner scanner = new Scanner(System.in);
-    public static void main(String[] args) {
-        System.out.println("Enter size of m: ");
-        int m = scanner.nextInt();
-        System.out.println("Enter size of n: ");
-        int n = scanner.nextInt();
-        int[][] matrix = new int[m][n];
-        int[][] secondMatrix = new int[n][m];
 
-        while(true) {
-            System.out.println("\n1. Create array \n2. Display Array \n3. Addition \n4. Multiplication");
-            System.out.println("\nEnter your choice: ");
+    public static Scanner scanner = new Scanner(System.in);
+    public static int row;
+    public static int col;
+    public static int[][] matrix;
+
+    public static void main(String[] args) {
+        while (true) {
+            System.out.println("\n1. Create Matrix");
+            System.out.println("2. Display Matrix");
+            System.out.println("3. Addition");
+            System.out.println("4. Multiplication");
+            System.out.println("5. Check Magical Matrix");
+            System.out.println("6. Check Prime Harmonic");
+            System.out.println("7. Exit");
+            System.out.print("Enter choice: ");
+
             int choice = scanner.nextInt();
+
             switch (choice) {
                 case 1:
-                    createMatrix(m, n, matrix);
+                    createMatrix();
                     break;
                 case 2:
-                    displayMatrix(m, n, matrix);
+                    displayMatrix(matrix);
                     break;
                 case 3:
-                     addition(m, n, matrix);
-                     break;
-                case 4:
-                    multiplication(m, n, matrix, secondMatrix);
+                    addition();
                     break;
+                case 4:
+                    multiplication();
+                    break;
+                case 5:
+                    boolean resultOfMagicalMatrix = checkMagicalMatrix(matrix);
+                    if (resultOfMagicalMatrix) {
+                        System.out.println("Given matrix is magical matrix");
+                    } else {
+                        System.out.println("Given matrix is not magical matrix");
+                    }
+                    break;
+                case 6:
+                    boolean resultOfPrimeHarmonic = checkPrimeHarmonic(matrix);
+                    if (resultOfPrimeHarmonic) {
+                        System.out.println("Given matrix is prime harmonic");
+                    } else {
+                        System.out.println("Given matrix is not prime harmonic");
+                    }
+                    break;
+                case 7:
+                    return;
                 default:
-                    continue;
+                    System.out.println("Invalid choice!");
             }
         }
     }
 
-    private static void createMatrix(int m, int n, int[][] matrix) {
-        System.out.println("Enter elements of matrix:");
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                System.out.print("matrix["+i+"]["+j+"]: ");
+    private static void createMatrix() {
+        System.out.print("Enter rows: ");
+        row = scanner.nextInt();
+        System.out.print("Enter columns: ");
+        col = scanner.nextInt();
+
+        matrix = new int[row][col];
+
+        System.out.println("Enter matrix elements:");
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
                 matrix[i][j] = scanner.nextInt();
             }
         }
     }
 
-    private static void displayMatrix(int m, int n, int[][] matrix) {
-        System.out.println("Matrix:");
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                System.out.print(matrix[i][j] + " ");
+    private static void displayMatrix(int[][] matrix) {
+        if (matrix == null) {
+            System.out.println("Matrix not created yet!");
+            return;
+        }
+
+        for (int[] row : matrix) {
+            for (int val : row) {
+                System.out.print(val + " ");
             }
             System.out.println();
         }
     }
-    private static void addition(int m, int n, int[][] matrix) {
-        int[][] additionMatrix = new int[m][n];
-        for (int i = 0; i<m; i++) {
-            for (int j = 0; j<n; j++) {
-                additionMatrix[i][j] = matrix[i][j] + matrix[i][j];
+
+    private static void addition() {
+        if (matrix == null) return;
+
+        int[][] result = new int[row][col];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                result[i][j] = matrix[i][j] + matrix[i][j];
             }
         }
-        displayMatrix(m, n, additionMatrix);
-    }
-    private static void multiplication(int m, int n, int[][] matrix, int[][] secondMatrix) {
-        createMatrix(n, m, secondMatrix);
-        int x = matrix.length;
-        int y = matrix[0].length;
-        int z = secondMatrix[0].length;
-        int[][] result = new int[x][z];
 
-        for (int i = 0; i < x; i++) {
-            for (int j = 0; j < z; j++) {
-                for (int k = 0; k < n; k++) {
+        System.out.println("Addition Result:");
+        displayMatrix(result);
+    }
+
+    private static void multiplication() {
+        if (matrix == null) return;
+
+        System.out.print("Enter columns for second matrix: ");
+        int secondMatrixCol = scanner.nextInt();
+
+        int[][] secondMatrix = new int[col][secondMatrixCol];
+
+        System.out.println("Enter second matrix elements:");
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < secondMatrixCol; j++) {
+                secondMatrix[i][j] = scanner.nextInt();
+            }
+        }
+
+        int[][] result = new int[col][secondMatrixCol];
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < secondMatrixCol; j++) {
+                for (int k = 0; k < col; k++) {
                     result[i][j] += matrix[i][k] * secondMatrix[k][j];
                 }
             }
         }
 
-        displayMatrix(x, z, result);
+        System.out.println("Multiplication Result:");
+        displayMatrix(result);
+    }
 
+    private static boolean checkMagicalMatrix(int[][] matrix) {
+        boolean flag = false;
+        if(row != col) {
+            System.out.println("For magic square, matrix must be of same row and column");
+            return false;
+        }
+
+        int initialRowSum = 0;
+        for (int i = 0; i < col; i++) {
+            initialRowSum += matrix[0][i];
+        }
+
+        int rowSum;
+        int colSum;
+        for (int i = 0; i < row; i++) {
+            rowSum = 0;
+            colSum = 0;
+            for (int j = 0; j < col; j++) {
+                rowSum += matrix[i][j];
+                colSum += matrix[j][row-1-i];
+            }
+            if (rowSum == initialRowSum && colSum == initialRowSum) {
+                flag = true;
+            } else {
+                flag = false;
+                return flag;
+            }
+        }
+
+        int diagonalSum = 0;
+        for (int j = 0; j < row; j++) {
+            diagonalSum += matrix[j][j];
+        }
+        if (diagonalSum == initialRowSum) {
+            flag = true;
+        } else {
+            flag = false;
+            return flag;
+        }
+
+        diagonalSum = 0;
+        for (int j = 0; j < row; j++) {
+            diagonalSum += matrix[j][row-1-j];
+        }
+        if (diagonalSum == initialRowSum) {
+            flag = true;
+        } else {
+            flag = false;
+            return flag;
+        }
+
+        return true;
+    }
+
+    private static boolean checkPrimeHarmonic(int[][] matrix) {
+        boolean flag = false;
+        if (matrix[0].length < 3) {
+            return false;
+        }
+        int primeCount = 0;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                boolean isPrime = checkPrime(matrix, i, j);
+                if (isPrime) {
+                    primeCount++;
+                }
+            }
+            if (primeCount == 3) {
+                flag = true;
+                primeCount=0;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean checkPrime(int[][] matrix, int i, int j) {
+        boolean isPrime = true;
+        if (matrix[i][j] == 0 || matrix[i][j] == 1) {
+            isPrime = false;
+            return isPrime;
+        }
+        for (int k = 2; k < matrix[i][j]/2; k++) {
+            if (matrix[i][j]%k == 0) {
+                isPrime = false;
+                return isPrime;
+            }
+        }
+        return isPrime;
     }
 }
+
