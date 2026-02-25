94d9b7d2a21c6406210fd7ef30c00279af452be2
divyarajsinhsindhav-tss <divyarajsinh.sindhav@tssconsultancy.com>
2026-02-25 16:21:06 +0530

    Random ID genrator

--- FoodOrderingConsoleApp/src/com/foodapp/utils/RandomID.java
+++ FoodOrderingConsoleApp/src/com/foodapp/utils/RandomID.java
@@ -3,9 +3,9 @@ package com.foodapp.utils;
 import java.util.Random;
 
 public class RandomID {
-    private Random rand = new Random();
+    private Random random = new Random();
 
     public int getRandomID() {
-        return rand.nextInt(900) + 100;
+        return random.nextInt(900) + 100;
     }
 }
